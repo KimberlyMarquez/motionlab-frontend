4470c029f56a6be27fac761b251593270eedaa84
mariana-im <marianaislasm2004@gmail.com>
2025-04-28 17:50:34 -0700

    Fix: Estilo de página de ParametrosIniciales

--- motionlab-frontend/src/components/Parametros.tsx
+++ motionlab-frontend/src/components/Parametros.tsx
@@ -7,11 +7,12 @@ interface Props {
     label: string;
     unidad: string;
     valorInicial: number;
+    step: number;
     min: number;
     max: number;
 }
 
-const ParametrosControl = ({ label, unidad, valorInicial, min, max}: Props) => {
+const ParametrosControl = ({ label, unidad, valorInicial, step, min, max}: Props) => {
     const [valor, setValor] = useState(valorInicial);
 
     const reset = () => setValor(valorInicial);
@@ -26,6 +27,7 @@ const ParametrosControl = ({ label, unidad, valorInicial, min, max}: Props) => {
                     type="range"
                     min={min}
                     max={max}
+                    step={step}
                     value={valor}
                     onChange={(e) => setValor(Number(e.target.value))}
                     className="slider"
@@ -33,7 +35,28 @@ const ParametrosControl = ({ label, unidad, valorInicial, min, max}: Props) => {
                 <input
                     type="number"
                     value={valor}
-                    onChange={(e) => setValor(Number(e.target.value))}
+                    onChange={(e) => {
+                        let newValue = e.target.value;
+                        
+                        if (newValue.includes(".")) {
+                          const [entero, decimales] = newValue.split(".");
+                          if (decimales.length > 2) {
+                            newValue = `${entero}.${decimales.slice(0, 2)}`;
+                          }
+                        }
+                    
+                        setValor(Number(newValue));
+                      }}
+                      onBlur={() => {
+                        if (valor < min) {
+                          alert(`El valor no puede ser menor a ${min}`);
+                          setValor(min);
+                        }
+                        if (valor > max) {
+                          alert(`El valor no puede ser mayor a ${max}`);
+                          setValor(max);
+                        }
+                      }}
                     className="input-box"
                 />
                 <button className="reset-btn" onClick={reset}>
--- motionlab-frontend/src/pages/ParametrosIniciales.tsx
+++ motionlab-frontend/src/pages/ParametrosIniciales.tsx
@@ -1,7 +1,7 @@
 import AjustesContainer from '../components/AjustesContainer';
 import Footer from '../components/Footer';
 import ButtonOrange from '../components/ButtonOrange';
-import Parametros from '../components/Parametros';
+import ParametrosControl from '../components/Parametros';
 import '../pages/Pages.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 
@@ -18,9 +18,9 @@ const ParametrosIniciales = () => {
         <div className="main-content">
           <AjustesContainer label="PARÁMETROS INICIALES" pag_anterior="/ajuste-equipos">
             <div className="d-flex flex-column align-items-center  mt-2">
-              <Parametros label="Revoluciones por minuto" unidad="rpm" valorInicial={2000} min={2000} max={4500}/>
-              <Parametros label="Tamaño de la rueda" unidad="cm" valorInicial={20} min={20} max={25}/>
-              <Parametros label="Distancia" unidad="m" valorInicial={5} min={5} max={22.6}/>
+              <ParametrosControl label="Revoluciones por minuto" unidad="rpm" valorInicial={2000} step={0.01} min={2000} max={4500}/>
+              <ParametrosControl label="Tamaño de la rueda" unidad="cm" valorInicial={20} step={0.01} min={20} max={25}/>
+              <ParametrosControl label="Distancia" unidad="m" valorInicial={5} step={0.01} min={5} max={22.6}/>
             </div>
 
             <div className="btn-orange text-center">
